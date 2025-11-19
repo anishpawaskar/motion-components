823a85de2255b038ff0ca8c3d60adc20fc9c7c17
anishpawaskar <anishpawaskar@gmail.com>
2025-11-19 15:58:07 +0530

    refactor/AceternityCard: add closing card feature

--- src/components/aceternity-card.tsx
+++ src/components/aceternity-card.tsx
@@ -2,11 +2,12 @@
 
 import React, { useState } from "react";
 import { ScreenWrapper } from "./common/screen-wrapper";
-import { CheckCircle, Clock, PlusIcon, Zap } from "lucide-react";
+import { CheckCircle, Clock, PlusIcon, XIcon, Zap } from "lucide-react";
 import { AnimatePresence, motion } from "motion/react";
 
 export const AceternityCard = () => {
   const [isHovered, setHovered] = useState(false);
+  const [open, setOpen] = useState(true);
   const items = [
     {
       id: 1,
@@ -36,73 +37,93 @@ export const AceternityCard = () => {
 
   return (
     <ScreenWrapper className="bg-neutral-100">
-      <motion.div
-        initial={{ opacity: 0, scale: 0.9, filter: "blur(10px)" }}
-        animate={{ opacity: 1, scale: 1, filter: "blur(0px)" }}
-        transition={{
-          duration: 0.5,
-          ease: "easeInOut",
-        }}
-        className="bg-white p-4 rounded-lg w-[320px] shadow-sm gap-4 flex flex-col"
-      >
-        <div>
-          <h3 className="text-lg font-semibold">Aceternity</h3>
-          <p className="text-neutral-500 text-sm">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod,
-            animi?
-          </p>
-        </div>
-        <div
-          onMouseEnter={() => setHovered(true)}
-          onMouseLeave={() => setHovered(false)}
-          className="w-full h-72 rounded-md border bg-neutral-100 border-neutral-200 overflow-hidden"
-        >
-          <AnimatePresence>
-            {isHovered && (
-              <motion.div
-                initial={{
-                  scale: 0.95,
-                  opacity: 0,
-                  filter: "blur(10px)",
-                }}
-                animate={{
-                  opacity: 1,
-                  scale: 1,
-                  filter: "blur(0px)",
-                }}
-                exit={{
-                  opacity: 0,
-                  scale: 0.95,
-                  filter: "blur(10px)",
-                }}
-                transition={{
-                  duration: 0.3,
-                  ease: "easeInOut",
-                }}
-                className="w-full h-full grid grid-rows-4 divide-y divide-neutral-200 bg-white"
-              >
-                {items.map((item) => {
-                  const IconComponent = item.Icon;
+      <AnimatePresence>
+        {open && (
+          <motion.div
+            initial={{ opacity: 0, scale: 0.9, filter: "blur(10px)" }}
+            animate={{ opacity: 1, scale: 1, filter: "blur(0px)" }}
+            exit={{ opacity: 0, scale: 0.9, filter: "blur(0px)" }}
+            transition={{
+              duration: 0.3,
+              ease: "easeInOut",
+            }}
+            className="bg-white p-4 rounded-lg w-[320px] shadow-sm gap-4 flex flex-col"
+          >
+            <div>
+              <div className="flex items-center justify-between">
+                <h3 className="text-lg font-semibold">Aceternity</h3>
+                <button
+                  onClick={() => setOpen(false)}
+                  className="size-4 flex items-center justify-center"
+                >
+                  <XIcon className="opacity-50" />
+                </button>
+              </div>
+              <p className="text-neutral-500 text-sm">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod,
+                animi?
+              </p>
+            </div>
+            <div
+              onMouseEnter={() => setHovered(true)}
+              onMouseLeave={() => setHovered(false)}
+              className="w-full h-72 rounded-md border bg-neutral-100 border-neutral-200 overflow-hidden"
+            >
+              <AnimatePresence>
+                {isHovered && (
+                  <motion.div
+                    initial={{
+                      scale: 0.95,
+                      opacity: 0,
+                      filter: "blur(10px)",
+                    }}
+                    animate={{
+                      opacity: 1,
+                      scale: 1,
+                      filter: "blur(0px)",
+                    }}
+                    exit={{
+                      opacity: 0,
+                      scale: 0.95,
+                      filter: "blur(10px)",
+                    }}
+                    transition={{
+                      duration: 0.3,
+                      ease: "easeInOut",
+                    }}
+                    className="w-full h-full grid grid-rows-4 divide-y divide-neutral-200 bg-white"
+                  >
+                    {items.map((item) => {
+                      const IconComponent = item.Icon;
 
-                  return (
-                    <div key={item.id} className="px-3 flex gap-4 items-center">
-                      <IconComponent className="shrink-0 opacity-40" />
-                      <div className="flex-1">
-                        <p className="text-sm font-semibold">{item.title}</p>
-                        <p className="text-sm text-neutral-500">{item.small}</p>
-                      </div>
+                      return (
+                        <div
+                          key={item.id}
+                          className="px-3 flex gap-4 items-center"
+                        >
+                          <IconComponent className="shrink-0 opacity-40" />
+                          <div className="flex-1">
+                            <p className="text-sm font-semibold">
+                              {item.title}
+                            </p>
+                            <p className="text-sm text-neutral-500">
+                              {item.small}
+                            </p>
+                          </div>
+                        </div>
+                      );
+                    })}
+                    <div className="flex px-3 gap-2 items-center justify-center">
+                      <PlusIcon className="opacity-50" />
+                      <p className="text-sm font-semibold">Create the future</p>
                     </div>
-                  );
-                })}
-                <div className="flex px-3 gap-2 items-center justify-center">
-                  <PlusIcon className="opacity-50" />
-                  <p className="text-sm font-semibold">Create the future</p>
-                </div>
-              </motion.div>
-            )}
-          </AnimatePresence>
-        </div>
-      </motion.div>
+                  </motion.div>
+                )}
+              </AnimatePresence>
+            </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </ScreenWrapper>
   );
 };
